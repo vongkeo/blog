a9ed9c2a24d768831ed928ee53eead5c7a647208
vongkeo <vongkeokeosavanh@gmail.com>
2020-05-09 11:10:27 +0700

    jwt token is done

--- app/User.php
+++ app/User.php
@@ -4,11 +4,12 @@ namespace App;
 
 use App\Model\blog;
 use App\Model\comment;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
@@ -45,5 +46,25 @@ class User extends Authenticatable
     public function comment()
     {
         return $this->hasMany(comment::class);
+    }
+
+     /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
